6036cd299b71590e75121845bc7892754b44381e
MayurBhowad <mayurbhowad1995@gmail.com>
2021-11-18 23:19:23 +0530

    started: templated routes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', require('./routes'))
+
+app.listen(PORT, () => console.log(`API is up and Rolling...`));
