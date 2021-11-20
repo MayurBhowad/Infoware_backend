4ab801031a0d9c43f4d101910d06117fe8d6cb1e
MayurBhowad <mayurbhowad1995@gmail.com>
2021-11-21 02:39:09 +0530

    fixed: response object

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,11 +3,17 @@ const auth = require('../middleware/auth.middleware');
 
 const router = require('express').Router();
 
+//@desc     get All products
+//@route    GET /api/product
+//access    public
 router.get('/', async (req, res) => {
     let products = await getAllProduct();
     res.send({ success: true, products: products })
 })
 
+//@desc     Add new product
+//@route    POST /api/product
+//access    private
 router.post('/', auth, (req, res) => {
     if (!req.user.isAdmin) {
         return res.status(403).json({ success: false, message: 'Access denied!' })
