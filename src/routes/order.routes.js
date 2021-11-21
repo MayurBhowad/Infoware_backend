863d4480c299a2de1f3511d14b5c03ba2ef4ca35
MayurBhowad <mayurbhowad1995@gmail.com>
2021-11-21 18:06:35 +0530

    added: new order swagger

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -13,6 +13,69 @@ router.get('/', (req, res) => {
 //@desc     new Order
 //@route    POST /api/order
 //access    private
+/**
+ * @swagger
+ * /api/order:
+ *      post:
+ *          summary: New order
+ *          tags: [Order]
+ *          security:
+ *              - token: ''
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          example: [
+ *                                      {
+ *                                        productid: 1001,
+ *                                        quantity: 2
+ *                                       },
+ *                                      {
+ *                                        productid: 1002,
+ *                                        quantity: 2
+ *                                       }
+ *                                  ]
+ *          responses:
+ *              200:
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              example:
+ *                                  success: true 
+ *                                  data: {
+ *                                           id: 1msdnn3j23sdf23,
+ *                                           userid: 1,
+ *                                           products: [
+ *                                                        {
+ *                                                          productid: 1001,
+ *                                                          quantity: 2
+ *                                                        },
+ *                                                        {
+ *                                                          productid: 1002,
+ *                                                          quantity: 2
+ *                                                        }
+ *                                                      ]
+ *                                        }
+ *              403:
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              example:
+ *                                  success: false 
+ *                                  message: "A token is required for authentication"
+ *              500:
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              example:
+ *                                  success: false 
+ *                                  message: "something went erong"
+ */
 router.post('/', auth, (req, res) => {
     /**
      * data=[
@@ -21,7 +84,8 @@ router.post('/', auth, (req, res) => {
      * ]
      */
 
-    newOrder(req.body.products, req.user.userid).then(response => {
+
+    newOrder(req.body, req.user.userid).then(response => {
         return res.json({ success: true, data: response.record })
     }).catch(err => {
         res.status(500).json({ success: false, message: 'server error' })
