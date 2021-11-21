043fb3473a12caa64a4054c99961c46016aeae43
MayurBhowad <mayurbhowad1995@gmail.com>
2021-11-21 17:26:07 +0530

    added: add new order

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,13 +1,19 @@
 const { newOrder } = require('../services/order.service');
+const auth = require('../middleware/auth.middleware');
 
 const router = require('express').Router();
 
-
+//@desc     test route
+//@route    GET /api/order
+//access    public
 router.get('/', (req, res) => {
     res.send({ msg: 'order router pass' })
 })
 
-router.post('/', (req, res) => {
+//@desc     new Order
+//@route    POST /api/order
+//access    private
+router.post('/', auth, (req, res) => {
     /**
      * data=[
      *  {productid: 10001, quantity: 3},
@@ -15,7 +21,7 @@ router.post('/', (req, res) => {
      * ]
      */
 
-    newOrder(req.body.products).then(response => {
+    newOrder(req.body.products, req.user.userid).then(response => {
         return res.json({ success: true, data: response.record })
     }).catch(err => {
         res.status(500).json({ success: false, message: 'server error' })
